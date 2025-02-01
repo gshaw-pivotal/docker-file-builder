b9633f8934a201704d00fa5df8d7b68747d0483e
Gavin Shaw <gavshaw+github@gmail.com>
2025-02-01 16:00:54 +1000

    Add ability to output content for node dockerfile

--- Dockerfile-Builder/Program.cs
+++ Dockerfile-Builder/Program.cs
@@ -1,3 +1,49 @@
 ﻿// See https://aka.ms/new-console-template for more information
+class DockerfileBuilder
+{
+    static void Main()
+    {
+        string lang;
 
-Console.WriteLine("Hello, World!");
+        Console.WriteLine("Enter Application Language:");
+        lang = Console.ReadLine().ToLower();
+
+        switch (lang)
+        {
+            case "node":
+            case "nodejs":
+            case "node.js":
+                int version = GetNodeVersion();
+                GenerateNodeDockerfile(version);
+                break;
+            default:
+                Console.WriteLine("Please enter a valid Application Language.");
+                break;
+        }
+    }
+
+    private static int GetNodeVersion()
+    {
+        Console.WriteLine("Enter Node Version (18 / 20 / 22 / 23):");
+        string version = Console.ReadLine();
+        return int.Parse(version);
+    }
+
+    private static void GenerateNodeDockerfile(int version)
+    {
+        Console.WriteLine("Creating Dockerfile...");
+        Console.WriteLine("======================");
+
+        Console.WriteLine($"FROM node:{version}");
+        Console.WriteLine("RUN mkdir -p /opt/app");
+        Console.WriteLine("WORKDIR /opt/app");
+        Console.WriteLine("COPY src/package.json src/package-lock.json .");
+        Console.WriteLine("RUN npm install");
+        Console.WriteLine("COPY src/ .");
+        Console.WriteLine("EXPOSE 3000");
+        Console.WriteLine("CMD [ \"npm\", \"start\"]");
+
+        Console.WriteLine("======================");
+        Console.WriteLine("Finished creating Dockerfile.");
+    }
+}
